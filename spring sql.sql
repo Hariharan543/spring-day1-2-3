3abb11a9597e04684ce4e6702ed7b3448d34e46a
Hariharan543 <37545181+Hariharan543@users.noreply.github.com>
2019-07-12 19:00:01 +0530

    Add files via upload

---
+++ spring sql.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE springDB;
+
+use springDB;
+
+
+
+
+create table Emp99
+
+(
+
+id int primary key NOT NULL AUTO_INCREMENT,
+
+name varchar(20),
+
+salary int,
+
+designation varchar(20)
+);
+
+select * from Emp99;
